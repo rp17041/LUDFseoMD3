81d85588a97fb1792bf029016d452560eb0c0298
Rihards Pluksis <rplukshis@gmail.com>
2021-03-19 17:22:01 +0200

    Content deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import Fideris from './components/Fideris';
+import Spinings from './components/Spinings';
+import Grunts from './components/Grunts';
+import Pludins from './components/Pludins';
+import Navigation from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/fideris" component={Fideris}/>
+             <Route path="/spiningosana" component={Spinings}/>
+             <Route path="/gruntsmakskeresana" component={Grunts}/>
+             <Route path="/pludinmakskeresana" component={Pludins}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
 }
-
+ 
 export default App;
